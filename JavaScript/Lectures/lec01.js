cfd2ef4400a345f81c01cbcb713469d61e6af011
shubhammgits <shubhamm18.work@gmail.com>
2025-02-04 00:23:18 +0530

    new lecture

---
+++ JavaScript/Lectures/lec01.js
@@ -0,0 +1,20 @@
+// alert("Hello")
+console.log("Shubham")
+
+let a = 5;
+let b = 10;
+console.log("Sum is " , a+b);
+
+let pencilPrice = 5;
+let penPrice = 10;
+
+// let output = "The total price is :" + (pencilPrice + penPrice) + "Rupees"
+// console.log(output)
+
+// or we can use template literals:
+
+let output = `The total price is : ${penPrice+pencilPrice} rupees`
+console.log(output)
+
+let age=18;
+console.log(age > 18)
