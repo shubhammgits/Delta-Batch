7dfe4d826605cfec8b8334cfd95b45a428df6305
shubhammgits <shubhamm18.work@gmail.com>
2025-02-17 12:48:43 +0530

    17feb

--- JavaScript/Lectures/Loops.js
+++ JavaScript/Lectures/Loops.js
@@ -1,11 +1,24 @@
-for(let i=1 ; i<=5 ; i++){
-    console.log(i)
-}
+// for(let i=1 ; i<=5 ; i++){
+//     console.log(i)
+// }
 
-for(let i=4 ; i>=1 ; i--){
-    console.log(i)
-}
+// for(let i=4 ; i>=1 ; i--){
+//     console.log(i)
+// }
 
-for(let i=10 ; i>=1 ; i-=3){
+// for(let i=10 ; i>=1 ; i-=3){
+//     console.log(i)
+// }
+
+
+// for(let i=1 ; i<=3 ; i++){
+//     for(let j=1 ; j<=3 ; j++){
+//         console.log(j)
+//     }
+// }
+
+let i=1
+while(i<=5){
     console.log(i)
+    i++
 }
