a96843223f7611f4c2eeb38078f9eb47986f7a77
shubhammgits <shubhamm18.work@gmail.com>
2025-02-17 14:01:18 +0530

    17feb

--- JavaScript/Lectures/Loops.js
+++ JavaScript/Lectures/Loops.js
@@ -27,6 +27,42 @@
 
 let fruits = ["mango" , "apple" , "banana" , "grapes" , "orange" , "litchi"]
 
-for(let i=0 ; i<fruits.length ; i++){
+for(let i=fruits.length-1 ; i>0 ; i--){
     console.log(i , fruits[i])
 }
+
+
+
+let heroes = [ ["ironman" , "thor" , "hulk" , "spiderman"] , ["superman" , "batman" , "wonder woman"]]
+
+for(let i=0 ; i<heroes.length ; i++){
+    
+    console.log(`List #${i}`)
+    for(let j=0 ; j<heroes[i].length ; j++){
+        console.log(heroes[i][j])
+    }
+}
+
+
+
+let student = [ ["shubham" , 9.2] , ["anshul" , 8.7] , ["john" , 8.2]]
+
+for(let i=0 ; i<student.length ; i++){
+    for(let j=0 ; j<student[i].length ; j++){
+        console.log(student[i][j])
+    }
+}
+
+
+for(char of "Shubham"){
+    console.log(char)
+}
+
+let names = [["shubham" , "india"] , ["jon" , "usa"] , ["putin" , "russia"]]
+for(i of names){
+    for(detail of i){
+        console.log(detail)
+    }
+}
+
+
