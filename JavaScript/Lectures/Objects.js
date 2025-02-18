680663573813088b7bb57aa539d84a05cc41a79b
shubhammgits <shubhamm18.work@gmail.com>
2025-02-18 23:55:15 +0530

    18feb

--- JavaScript/Lectures/Objects.js
+++ JavaScript/Lectures/Objects.js
@@ -25,3 +25,98 @@ console.log(post)
 console.log(post.username)
 console.log(post.followers)
 console.log(post.tags[1])
+
+
+const obj = {
+    1 : "a",
+    2 : "b",
+    true : "c",
+    null : "d",
+    undefined : "e"
+}
+
+
+const student3 = {
+    name : "Shubham",
+    age : 18,
+    cgpa : 9.5,
+    city : "Delhi"
+}
+
+student3.age = 21
+student3.gender = "Male"
+delete student3.city
+console.log(student3)
+
+
+
+
+//Nested
+
+const classInfo = {
+    shubham : {
+        age : 21,
+        cgpa : 9.2,
+        city : "Delhi"
+    },
+
+    anshul : {
+        age : 22,
+        cgpa : 8.3,
+        city : "Mumbai"
+    },
+
+    ankit : {
+        age : 20,
+        cgpa : 9.5,
+        city : "Kolkata"
+    }
+}
+
+
+classInfo.shubham.age = 18
+
+console.log(classInfo)
+console.log(classInfo.ankit)
+console.log(classInfo.shubham.cgpa)
+
+
+
+//Array of objects
+
+const info = [
+    {
+        name : "Shubham",
+        age : 21,
+        cgpa : 9.2,
+        city : "Delhi"
+    },
+
+    {
+        name : "Anshul",
+        age : 22,
+        cgpa : 8.3,
+        city : "Mumbai"
+    },
+
+    {
+        name : "Ankit",
+        age : 20,
+        cgpa : 9.5,
+        city : "Kolkata"
+    }
+]
+
+console.log(info)
+
+info.push({
+    name : "Aniket",
+    age : 19,
+    cgpa : 9.0,
+    city : "Chennai"
+})
+
+console.log(info)
+console.log(info[0].name)
+
+info[1].name = "Anshul Gupta"
